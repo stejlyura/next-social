5003185220759c4efeca6073101f2af4ad393ab6
YuriiM <stejlyura19@gmail.com>
2025-08-25 17:54:35 +0300

    Add Nav component and implement API hooks for user and post data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,26 @@
+"use client"
+import Nav from "./templates/Nav";
+import { useAxiosAPI } from "@/hooks/axiosAPI";
+import type { Post } from "@/interfaces/postInterface";
 
 export default function Home() {
+    const { data, loading, error } = useAxiosAPI<Post[]>('https://jsonplaceholder.typicode.com/posts')
+
   return (
-    <>
-      <h1>Hellom world</h1>
-    </>
+    <div className="flex h-screen">
+      <div className="w-1/5 bg-gray-50 h-full">
+        <Nav/>
+      </div>
+      <div className="w-4/5 bg-gray-30 h-full overflow-y-auto p-6">
+        <ul>
+          {data?.map((u)=>(
+            <li key={u.id}>
+              <h2>{u.title}</h2>
+              <p>{u.body}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
---
+++ src/app/templates/Nav.tsx
@@ -0,0 +1,35 @@
+"use clien"
+
+import React from 'react'
+import { useAxiosAPI } from '@/hooks/axiosAPI'
+import type { User } from '@/interfaces/userInterface'
+
+function Nav() {
+  const { data, loading, error } = useAxiosAPI<User>('https://jsonplaceholder.typicode.com/users/1')
+
+
+
+
+  return (
+    <nav className="h-full w-full flex flex-col items-center justify-start bg-[var(--foreground)] text-white">
+        <ul className='w-full p-2'>
+            <li><div className='bg-gray-500 border rounded-2xl w-10 h-10'></div></li>
+            <li>
+              <h1>
+                {loading
+                  ? "User name"
+                  : data?.name
+                }
+              </h1>
+            </li>
+        </ul>
+        <ul className='w-full p-2'>
+            <li><a href="/">Home</a></li>
+            <li><a href="/Friends">Friends</a></li>
+            <li><a href="/Discovering">Discovering</a></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Nav
