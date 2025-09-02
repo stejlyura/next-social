92ffcaa6170167cd1cac543522bfecb05f7ff5bf
YuriiM <stejlyura19@gmail.com>
2025-09-02 23:44:10 +0300

    Add emoji

--- src/elements/FriendsCard.tsx
+++ src/elements/FriendsCard.tsx
@@ -16,10 +16,10 @@ export const FriendsCard = ({userId, name, email,city, street, phone }: FriendsC
   return (
     <div className='w-full '>
         <h1 className='text-2xl'><a href={`/user-page/${userId}`}>{name}</a></h1>
-        <p className='text-gray-500  before:content-["🔥"] before:mr-1'>{email}</p>
-        <p>{city}</p>
-        <p>{street}</p>
-        <p>{phone}</p>
+        <p className='text-gray-500  before:content-["📧"] before:mr-1'>{email}</p>
+        <p className='before:content-["🏙️"] before:mr-1'>{city}</p>
+        <p className='before:content-["🏠"] before:mr-1'>{street}</p>
+        <p className='before:content-["📱"] before:mr-1'>{phone}</p>
     </div>
   )
 }
